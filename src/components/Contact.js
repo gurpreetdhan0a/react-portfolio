da574886f4a75fc6d83e4e8781744d5060ebb9bb
Gurpreet Singh <gurpreet.dhanoa@hotmail.com>
2021-06-10 17:15:46 +1000

    Contact form styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button, Paper } from '@material-ui/core';
 const Contact = () => {
+    const useStyles = makeStyles((theme) => ({
+        multilineColor:{
+            color:'white'
+        }
+    }));
+    const classes = useStyles();
     return (
         <div id="contact" className="portfolio">
             <h1>Contact</h1>
             <div className="color">
             <Paper>
             <form name="contact" netlify>
-            <TextField required name="name" variant="outlined" label="Name" fullWidth type="text"/>
+            <TextField required name="name" variant="outlined" label="Name" fullWidth type="text" InputProps={{
+            className: classes.multilineColor}}/>
 
-            <TextField style={{marginTop: "10px", marginBottom: "10px"}} required name="email" type="email" variant="outlined" label="Email" fullWidth value=""/>
+            <TextField style={{color:"white",marginTop: "10px", marginBottom: "10px"}} required name="email" type="email" variant="outlined" label="Email" InputProps={{
+            className: classes.multilineColor}} fullWidth/>
 
-            <TextField style={{marginTop: "10px"}} required name="message" variant="outlined" label="Message" fullWidth type="text"/>
+            <TextField style={{marginTop: "10px"}} required name="message" variant="outlined" InputProps={{
+            className: classes.multilineColor}} label="Message" fullWidth type="text"/>
 
             <Button style={{marginTop: "10px", }}variant="contained" color="secondary" size="large" type="submit" fullWidth >Submit</Button>
             </form>
