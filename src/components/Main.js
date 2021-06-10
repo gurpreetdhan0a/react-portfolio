336a5b228000df90986fff6a8ace8a8ef1348d22
Gurpreet Singh <gurpreet.dhanoa@hotmail.com>
2021-06-10 16:07:54 +1000

    Portfolio finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
     return (
         <div className="main">
             <p>WELCOME TO MY PORTFOLIO</p>
-            <h1>Hi, I'm Gurpreet , <br/><p className="text animation">A Web Developer</p></h1>
+            <h1>Hi, I'm Gurpreet, <br/><p className="text animation">A Web Developer</p></h1>
             <div className="social-icons">
                 <a href="https://www.linkedin.com/in/gurpreetsinghdhanoa/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
                 <a href="https://github.com/gurpreetdhan0a?tab=repositories" target="_blank" rel="noopener noreferrer">
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,10 +11,11 @@ import portfolio5 from '../assets/images/portfolio-5.png';
 import portfolio6 from '../assets/images/portfolio-6.png';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkIcon from '@material-ui/icons/Link';
+import Chip from '@material-ui/core/Chip';
 const useStyles = makeStyles({
     media: {
-      height: 210,
-      padding:"10px",
+      height: 290,
+      padding:"-20px",
       border: "1px solid white",
       borderRadius : "2%",
     }
@@ -51,14 +52,22 @@ const Portfolio = () => {
                         <Typography color="primary" gutterBottom variant="h4">
                             MiniBlog
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="React.js" />
+                        <Chip label="Redux" />
+                        <Chip label="React-Router" />
+                        <Chip label="MaterialUI" />
+                        <Chip label="Node.js" />
+                        <Chip label="Express" />
+                        <Chip label="MongoDB" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            MiniBlog is an web app where you can post small blog using your Google Id, you need to be signed in to delete, update, like and post the blog. Logging out will change the React state to null and the functioanlity will be lost.
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/miniblog-frontend" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="https://miniblog-lyart.vercel.app/allPosts" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
@@ -74,14 +83,21 @@ const Portfolio = () => {
                         <Typography color="primary" gutterBottom variant="h4">
                             E-commerce
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="React.js" />
+                        <Chip label="React-Router" />
+                        <Chip label="Redux" />
+                        <Chip label="HTML/CSS" />
+                        <Chip label="SemanticUi" />
+                        <Chip label="JavaScript" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            E-commerce is a website which uses redux state management system to store items in the cart. It reads information about products from the db.json file and prints the collection on the screen. Also, it uses react-router to send information to components.
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/ecommerce-react" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="https://ecommerce-react-sage.vercel.app/" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
@@ -97,14 +113,19 @@ const Portfolio = () => {
                         <Typography color="primary" gutterBottom variant="h4">
                             Rented
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="React.js" />
+                        <Chip label="HTML/CSS" />
+                        <Chip label="SemanticUi" />
+                        <Chip label="JavaScript" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            Rennted.com is a combination of <u>realestate.com.au</u> and <u>snug.com.au</u>. One is basically seraching for properties and the other is for the application process. Rented.com uses Google Authentication to identify user and submit the application. 
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/rented" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="https://rented.vercel.app/" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
@@ -118,16 +139,20 @@ const Portfolio = () => {
                     </CardActionArea>
                     <CardContent>
                         <Typography color="primary" gutterBottom variant="h4">
-                            OOP Game App
+                            Phrase Hunter
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="HTML/CSS" />
+                        <Chip label="JavaScript" />
+                        <Chip label="Anime.js" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            Phrase Hunter uses Object-oriented JavaScript fundamentals and uses class and method structure to validate the keywords and to show the random phrases. Also, it uses Anime.js for animations of the letters.
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/oopGameApp-Project-4" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="http://oopgameapp.online/" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
@@ -141,16 +166,19 @@ const Portfolio = () => {
                     </CardActionArea>
                     <CardContent>
                         <Typography color="primary" gutterBottom variant="h4">
-                            MiniBlog
+                            Form Validation
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="HTML/CSS" />
+                        <Chip label="JavaScript" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            This projects uses JavaScript to validate different inputs like Email, Credit-card, Checkbox, Dropdown selectors and much more and provides helper message using Regex.
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/interactiveForm-Project-3" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="http://interactiveform.online/" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
@@ -166,14 +194,17 @@ const Portfolio = () => {
                         <Typography color="primary" gutterBottom variant="h4">
                             List and Pagination
                         </Typography>
-                        <Typography variant="body1" color="primary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
+                        <div className="tags">
+                        <Chip label="HTML/CSS" />
+                        <Chip label="JavaScript" />
+                        </div>
+                        <Typography variant="h6" color="primary">
+                            This project uses JavaScript to take "n" number of students and convert them into pages which is 10 students per page. You can also search for students by their names.
                         </Typography>
                     </CardContent>
                     <CardActions className="icons">
-                        <a href="https://github.com/"><GitHubIcon color="primary"/></a>
-                        <a href="https://github.com/"><LinkIcon color="primary"/></a>
+                        <a href="https://github.com/gurpreetdhan0a/listPaginationAndFiltering-Project-2" target="_blank" rel="noopener noreferrer" ><GitHubIcon color="primary"/></a>
+                        <a href="http://listpaginationandfiltering.online/" target="_blank" rel="noopener noreferrer" ><LinkIcon color="primary"/></a>
                     </CardActions>
                 </Card>
                 </div>
