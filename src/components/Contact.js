576986b0c5e1393ecb3459cd46bb6aab27e2b419
Gurpreet Singh <gurpreet.dhanoa@hotmail.com>
2021-06-12 16:10:09 +1000

    contact form and media queries update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
             <h1>Contact</h1>
             <div className="color">
             <Paper>
-            <form name="contact" netlify>
+            <form name="contact" method="POST" date-netlify="true">
             <TextField required name="name" variant="outlined" label="Name" fullWidth type="text" InputProps={{
             className: classes.multilineColor}}/>
 
